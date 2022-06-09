04b8fa9d2720c429790ef446e9f3e964d6b9d944
unanchoi <unusualc@likelion.org>
2022-06-09 20:44:32 +0900

    Initial commit

---
+++ src/main/java/com/demo/unan/UnanApplication.java
@@ -0,0 +1,13 @@
+package com.demo.unan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/unan/UnanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.unan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
