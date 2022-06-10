adfcceff5c87aea4eadcd7897e25a16d0202f711
unanchoi <unusualc@likelion.org>
2022-06-11 02:13:55 +0900

    feat: Add PostsSaveRequestDto

---
+++ src/main/java/com/demo/unan/web/dto/PostsSaveRequestsDto.java
@@ -0,0 +1,33 @@
+package com.demo.unan.web.dto;
+
+
+import com.demo.unan.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.swing.text.html.parser.Entity;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestsDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestsDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
