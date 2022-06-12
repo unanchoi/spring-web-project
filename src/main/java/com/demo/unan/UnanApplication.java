b91d81ee023acea96156c3cc02c2c99b03612b47
unanchoi <unusualc@likelion.org>
2022-06-12 23:25:51 +0900

    feat: Add JPA Auditing 활성화

--- src/main/java/com/demo/unan/UnanApplication.java
+++ src/main/java/com/demo/unan/UnanApplication.java
@@ -2,7 +2,9 @@ package com.demo.unan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // jpa Auditing 활성화
 @SpringBootApplication
 public class UnanApplication {
 
