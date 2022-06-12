76caa72112069cabbfaea5f1c6f2c2bba65959f2
unanchoi <unusualc@likelion.org>
2022-06-13 00:10:42 +0900

    feat: Add index controller

--- src/main/java/com/demo/unan/web/IndexController.java
+++ src/main/java/com/demo/unan/web/IndexController.java
@@ -1,2 +1,15 @@
-package com.demo.unan.web;public class IndexController {
+package com.demo.unan.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
