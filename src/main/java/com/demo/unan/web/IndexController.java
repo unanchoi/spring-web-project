47d2efd957f45b078c9223706b654cda06ed8791
unanchoi <unusualc@likelion.org>
2022-06-13 00:39:43 +0900

    fix: index controller annotation 변경

--- src/main/java/com/demo/unan/web/IndexController.java
+++ src/main/java/com/demo/unan/web/IndexController.java
@@ -1,15 +1,14 @@
 package com.demo.unan.web;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
 }
