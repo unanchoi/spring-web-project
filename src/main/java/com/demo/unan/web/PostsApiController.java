f02b42307fabe9eaa7e63ae523fa5b89024f28f5
unanchoi <unusualc@likelion.org>
2022-06-14 00:58:53 +0900

    fix: Fix typo

--- src/main/java/com/demo/unan/web/PostsApiController.java
+++ src/main/java/com/demo/unan/web/PostsApiController.java
@@ -1,6 +1,6 @@
 package com.demo.unan.web;
 
-import com.demo.unan.services.posts.PostService;
+import com.demo.unan.services.posts.PostsService;
 import com.demo.unan.web.dto.PostsResponseDto;
 import com.demo.unan.web.dto.PostsSaveRequestsDto;
 import com.demo.unan.web.dto.PostsUpdateRequestDto;
@@ -10,23 +10,23 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
-    private final PostService postService;
+    private final PostsService postsService;
 
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestsDto requestsDto) {
-        return postService.save(requestsDto);
+        return postsService.save(requestsDto);
     }
 
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id,
                        @RequestBody PostsUpdateRequestDto requestDto){
-        return postService.update(id, requestDto);
+        return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
-        return postService.findById(id);
+        return postsService.findById(id);
     }
 }
