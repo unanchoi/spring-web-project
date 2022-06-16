1164a900c4f69a94f2ee6f028a5c7c65da58a66b
unanchoi <unusualc@likelion.org>
2022-06-17 00:13:39 +0900

    feat: Add logic for adding user name to model index.mustach에서 userName을 사용하기 위한 기능 추가

--- src/main/java/com/demo/unan/web/IndexController.java
+++ src/main/java/com/demo/unan/web/IndexController.java
@@ -1,20 +1,30 @@
 package com.demo.unan.web;
 
+import com.demo.unan.config.auth.dto.SessionUser;
 import com.demo.unan.services.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
