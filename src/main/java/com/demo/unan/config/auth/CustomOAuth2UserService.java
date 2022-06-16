55cecd8cff29c84a0952e9fe757014414c1e3b9d
unanchoi <unusualc@likelion.org>
2022-06-16 23:00:09 +0900

    Add oauthattributes dto

--- src/main/java/com/demo/unan/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/demo/unan/config/auth/CustomOAuth2UserService.java
@@ -52,6 +52,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
                 .orElse(attributes.toEntity());
 
-        return userRepository.save(user)
+        return userRepository.save(user);
     }
 }
